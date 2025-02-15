34008c187616c150a39b0216228d24cca55ab1e5
Mark1Lessey <mark.lessey1@my.uwi.edu>
2025-02-15 17:56:05 -0400

    Base HTML without grid layout

---
+++ categories.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>EduPlay</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fontdiner+Swanky&display=swap">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <div class="head-nav">
+                <div class = "center">
+                    <a class="game-name" href="index.html">Jeopardy</a></li>
+                </div>
+                <div class="left">
+                    <a class="left-arrow" href="#"><span>&#129120;</span></a>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="game-board">
+                    <div class="category">
+                        <?php 
+                            $categories = array("Category 1", "Category 2", "Category 3", "Category 4", "Category 5");
+                            $questions = array(100, 200, 300, 400, 500);
+                            for ($i = 0; $i < 5; $i++) {
+                                echo "<div class='category-title'><a href='categoryedit.php'>" . $categories[$i] . "</a></div>";
+                            }
+                        ?>
+                    </div>
+                    
+                    <div class="question">
+                        <?php
+                            echo "<br>";
+                            for ($i = 0; $i < 5; $i++) {
+                                echo "<div class='question'><div class='question-value'>$" . $questions[$i] . "</div>";
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <p>&copy; 2025 EduPlay</p>
+        </footer>
+    </body>
+</html>
