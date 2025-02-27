ea7b54951db995d70cce327d49d80f15a4a93c19
Mark1Lessey <mark.lessey1@my.uwi.edu>
2025-02-27 19:03:24 -0400

    Pushed Changes for the app removal

--- flask-server/App/models/Game.py
+++
@@ -1,5 +0,0 @@
-# from App.config import db
-
-# class Game(db.Model):#TODO Make this an abstract class or future games to inherit from
-#     id = db.Column(db.String(60),primary_key=True)
-#     questions = db.relationship("Question")
--- flask-server/App/models/GaneQuestion.py
+++
@@ -1,3 +0,0 @@
-# from App.config import db #Remove Comments from the files after correct modelling is done
-
-# class GameQuestion(db.Model):
