970b83437980d2fd075b4b09ac2d0b38c8c0e745
Sajdah <64163797+sajdah21@users.noreply.github.com>
2025-03-06 23:21:14 -0400

    implemented csv functionality so that the user is able to upload a csv file (that meets the criteria) and stores a game of jeopardy in the db

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,11 +81,8 @@ define(['./workbox-3ddb0163'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "registerSW.js",
-    "revision": "3ca0b8505b4bec776b69afdba2768812"
-  }, {
     "url": "index.html",
-    "revision": "0.198fud549vg"
+    "revision": "0.rakb0vg7luo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
