8e4f0966e2e1913ef6d104f3445f7f65e482ded3
che.salazar <che.salazar@my.uwi.edu>
2025-03-07 07:35:58 -0400

    #31 Review Modal UI Fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-3ddb0163'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ui141ioh8"
+    "revision": "0.65qm95q0kqo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
