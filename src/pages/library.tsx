926f96fc41754092a17d69ba3c66e7b02cf48215
Sajdah <64163797+sajdah21@users.noreply.github.com>
2025-04-14 03:00:07 -0400

    updated the library page design!

--- src/pages/library.tsx
+++ src/pages/library.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./../styles/library.css"; 
-import ImageName from "/assets/images/roleplay.png"
 import Library from "/assets/images/bookshelf.png"
 import Home from "/assets/images/house.png"
 import GameList from "../components/game-list";
@@ -11,48 +10,44 @@ import GameList from "../components/game-list";
 const GameComponent: React.FC = () => {
   sessionStorage.clear()
 
- 
-
   return (
     <div className="container">
       <div className="header">
         <div className="topnav">
-          <input type="text" placeholder="Search.." />
+          <input type="text" placeholder="Search..." />
         </div>
       </div>
 
       <div className="categories">
-    <span>
-      <img src={ImageName} alt="Image" />
-      <img src={ImageName} alt="Image" />
-      <img src={ImageName} alt="Image" />
-      <img src={ImageName} alt="Image" />
-      <img src={ImageName} alt="Image" />
-      <img src={ImageName} alt="Image" />
-    </span>
+        <Link  to={"/csv"}>
+          <button className="sidebar-button">
+           Import questions
+          </button>
+        </Link>
+  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+
+
+        <Link to = "/PreviousGames">
+          <button className="sidebar-button">
+            Previous Games
+          </button>
+        </Link>
 
       </div>
 
       <div className="menu">
-        <Link  to={"/csv"}>
-              <button className="sidebar-button">
-                Import questions
-              </button>
-        </Link>
-        <br/>
-        <br/>
-        <Link to = "/PreviousGames">
-              <button className="sidebar-button">
-                Previous Games
-              </button>
-        </Link>
         <br />
-        <br />
+        <hr />
+                <Link  to={"/home"}>
+
         <img src={Home} alt="Image" width={40} height={40} />
         <p>Home</p>
+        </Link>
         <br />
         <img src={Library} alt="Image" />
         <p>Library</p>
+
         <br />
         <hr />
         <br />
