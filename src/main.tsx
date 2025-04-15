c234aede8f4be45a4055dc8fa57725d3c5b80d21
Flaxere <123189890+Flaxere@users.noreply.github.com>
2025-04-15 01:26:37 -0400

    removed duplicate toasts

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ReloadPrompt from './components/reload-prompt.tsx'
+import { Toaster } from 'sonner'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster richColors position='top-right' />
     <App />
     <ReloadPrompt />
   </StrictMode>,
